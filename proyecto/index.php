6437cb237a9ddd3acc6a02ed25617f6fc5790fef
Carla Rodríguez <carlardgz17@gmail.com>
2023-03-06 16:10:23 -0600

    commit

--- proyecto/index.php
+++ proyecto/index.php
@@ -1,14 +1,3 @@
 <?php
-$servername = "148.213.1.131:3306";
-$database = "mydb";
-$username = "root";
-$password = "mi-contraseña-segura";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($conn);
+echo "Login Federado. María José Ávila Amezcua, Fabián Daniel Preciado Martínez, Carla Rodríguez Aguilar" 
 ?>
