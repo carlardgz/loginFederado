e0763ba3b0d6aaf083679c3cc4caf3ee6b10df13
Carla Rodríguez <carlardgz17@gmail.com>
2023-03-06 11:39:37 -0600

    tercer commit

--- proyecto/simplesaml/config/authsources.php
+++ proyecto/simplesaml/config/authsources.php
@@ -40,7 +40,7 @@ $config = [
 
         // The entity ID of the IdP this SP should contact.
         // Can be NULL/unset, in which case the user will be shown a list of available IdPs.
-        'idp' => "datacenter-idp",
+        'idp' => "http://wayf.ucol.mx",
 
         // The URL to the discovery service.
         // Can be NULL/unset, in which case a builtin discovery service will be used.
